28f1cf0cd341f490b62c11e3a3adb28862adca2c
onyxgeek <onyxpeek@gmail.com>
2023-10-18 18:25:36 +0300

    fix: fix the repaint function

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -20,21 +20,22 @@ public class GamePanel extends JPanel{
 	}
 	public void changeXDelta(int value) {
 		this.xDelta += value;
-		repaint();
+		
 	}
 	
 	public void changeYDelta(int value) {
 		this.yDelta += value;
-		repaint();
+		
 	}
 	public void setRectPos(int x, int y) {
 		this.xDelta = x;
 		this.yDelta = y;
-		repaint();
+		
 	}
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
 		g.fillRect(xDelta, yDelta, 200, 50);
+		repaint();
 	}
 }
